6e2c8595ad6c4990fee82f0ccad949a38074ff54
mythosmystery <axiysfire@gmail.com>
2021-10-24 17:12:36 -0500

    updated bug card

--- client/src/components/BugCard.js
+++ client/src/components/BugCard.js
@@ -3,7 +3,9 @@ import { Card, Button } from 'react-bootstrap';
 import RemoveBugButton from './RemoveBugButton';
 import EditStatusButton from './EditStatusButton';
 import EditBugButton from './EditBugButton';
+import moment from 'moment';
 const BugCard = ({ bug }) => {
+   const datePosted = moment(bug.date).format('hh:mm a');
    return (
       <Card className="my-2">
          <Card.Header>
@@ -14,9 +16,10 @@ const BugCard = ({ bug }) => {
             <Card.Text>{bug.description}</Card.Text>
             <Card.Text>{bug.replicate}</Card.Text>
             <Card.Text>{bug.errorMessage}</Card.Text>
+            <Card.Text>Version: {bug.version}</Card.Text>
          </Card.Body>
          <Card.Footer>
-            Version: {bug.version}
+            Reported: {datePosted}
             <EditBugButton bug={bug}></EditBugButton>
             <RemoveBugButton bugId={bug._id}></RemoveBugButton>
          </Card.Footer>
